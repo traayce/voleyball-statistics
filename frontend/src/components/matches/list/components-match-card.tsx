840e9218da3468c1399e942037e99cae75afcc4f
traayce <justin.rackauskas@gmail.com>
2019-05-04 16:27:49 +0300

    styling matches list

--- frontend/src/components/matches/list/components-match-card.tsx
+++ frontend/src/components/matches/list/components-match-card.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
-import { Card, CardActionArea, CardContent, Typography, CardActions, Button } from "@material-ui/core";
+import { Card, CardActionArea, CardContent, Typography, CardActions, Button, Grid } from "@material-ui/core";
+import People from "@material-ui/icons/People";
 import { MatchModel } from "src/types";
 import { createStyles, Theme, WithStyles, withStyles } from "@material-ui/core/styles";
+import moment = require("moment");
 
 const styles = (theme: Theme) => createStyles({
     card: {
@@ -15,19 +17,49 @@ interface OwnProps {
     match: MatchModel;
 }
 type Props = OwnProps & WithStyles<typeof styles>;
+
 class MatchCardClass extends React.Component<Props> {
     public render(): JSX.Element {
         const { openEditor, match, classes } = this.props;
+        const time = moment(match.startsAt).format("YYYY/MM/DD HH:mm");
         return <Card className={classes.card} key={match.id}>
             <CardActionArea onClick={openEditor(match)}>
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        Varžybos {match.teamA.name} - {match.teamB.name}
+                    <Grid container direction="column" justify="center" xs={12}>
+                        <Grid container xs={12} justify="center">
+                            <Typography component="h4" variant="caption">
+                                {time}
+                            </Typography>
+                        </Grid>
+                        <Grid container xs={12} justify="center">
+                            <Typography component="h4" variant="caption">
+                                {match.location}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid
+                        container
+                        justify="center"
+                        xs={12}>
+                        <Grid container xs={6}
+                            justify="center">
+                            <Grid container xs={12} justify="center" >
+                                <People fontSize="large" />
+                            </Grid>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {match.teamA.name}  ({match.pointsSummary.teamASetPoints})
                     </Typography>
-                    <Typography component="p">
-                        Vieta: {match.location}
-                        Laikas: {match.startsAt}
+                        </Grid>
+                        <Grid container xs={6}
+                            justify="center">
+                            <Grid container xs={12} justify="center" >
+                                <People fontSize="large" />
+                            </Grid>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {match.teamB.name}  ({match.pointsSummary.teamBSetPoints})
                     </Typography>
+                        </Grid>
+                    </Grid>
                 </CardContent>
             </CardActionArea>
             <CardActions>
--- frontend/src/components/matches/list/components-matches-styles.ts
+++ frontend/src/components/matches/list/components-matches-styles.ts
@@ -2,12 +2,7 @@ import { createStyles, Theme } from "@material-ui/core/styles";
 
 export const MatchesContainerStyles = (theme: Theme) => createStyles({
     Container: {
-        width: "95%",
         margin: "1em auto 0",
-        padding: "1em",
-        flexGgrow: 1,
-        minHeight: "400px",
-        paddingBottom: "2em",
     },
     TextField: {
         marginLeft: theme.spacing.unit,
